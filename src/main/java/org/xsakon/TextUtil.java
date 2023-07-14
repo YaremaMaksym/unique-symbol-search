fc6beb6edcaa3760493c724dddaad30b0a8887fa
Maksym Yarema <macsym2@gmail.com>
2023-07-14 15:32:19 +0300

    feat: new method findWords() in new class TextUtil

---
+++ src/main/java/org/xsakon/TextUtil.java
@@ -0,0 +1,35 @@
+package org.xsakon;
+
+import java.util.ArrayList;
+
+public class TextUtil {
+
+    public ArrayList<String> findWords(java.lang.String text) {
+        ArrayList<String> words = new ArrayList<>();
+        String word = "";
+
+
+        for (int i = 0; i < text.length(); i++) {
+
+            char currentChar = text.charAt(i);
+            if ((currentChar >= 65 && currentChar <= 90)
+                    || (currentChar >= 96 && currentChar <= 120)){
+
+                word = word.concat(Character.toString(currentChar));
+
+
+                char nextChar = text.charAt(i + 1);
+                if (((nextChar < 65 || nextChar > 90)
+                        && (nextChar < 96 || nextChar > 120))) {
+
+                    words.add(word);
+                }
+            }
+            else{
+                word = "";
+            }
+        }
+
+        return words;
+    }
+}
